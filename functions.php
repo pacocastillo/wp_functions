ecd64aa7541774af4b813666b89d099b8666f0d3
Francisco Castillo Gómez <castillo.gomez82@gmail.com>
2015-06-19 20:46:02 +0200

    Añadir custom field a un pedido woocommerce

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php 
-
+// Subir imagen a wordpress
 function uploadRemoteImageAndAttach($image_url, $parent_id = null){
   $image = $image_url;
   $get = wp_remote_get( $image );
@@ -17,5 +17,32 @@ function uploadRemoteImageAndAttach($image_url, $parent_id = null){
   $attach_data = wp_generate_attachment_metadata( $attach_id, $mirror['file'] );
   wp_update_attachment_metadata( $attach_id, $attach_data );
   return $attach_id;
+}
+
+// Añadir campo adicional a un usuario en woocommerce y que aparezca en el pedido
+add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
+// Our hooked in function - $fields is passed via the filter!
+function custom_override_checkout_fields( $fields ) {
+  $fields['billing']['nif'] = array(
+      'label'     => __('NIF', 'woocommerce'),
+  'placeholder'   => _x('NIF', 'placeholder', 'woocommerce'),
+  'required'  => true,
+  'class'     => array('form-row-wide'),
+  'clear'     => true
+  );
+
+  return $fields;
+}
+
+add_action( 'woocommerce_checkout_update_order_meta', 'my_custom_checkout_field_update_order_meta' );
+function my_custom_checkout_field_update_order_meta( $order_id ) {
+    if ( ! empty( $_POST['nif'] ) ) {
+        update_post_meta( $order_id, 'user_nif', sanitize_text_field( $_POST['nif'] ) );
+    }
+}
+
+add_action( 'woocommerce_admin_order_data_after_billing_address', 'my_custom_checkout_field_display_admin_order_meta', 10, 1 );
+function my_custom_checkout_field_display_admin_order_meta($order){
+    echo '<p><strong>'.__('NIF').':</strong> ' . get_post_meta( $order->id, 'user_nif', true ) . '</p>';
 }
 ?>
